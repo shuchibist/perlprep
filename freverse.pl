a302aa8b9d5fbc2c13e3a612712af13f823df8b3
shuchi <shuchi@TWiZTTED.(none)>
2012-10-24 16:50:34 +0530

    my first perl script

---
+++ freverse.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+open OUTPUT, '>rever1234' or die "$!";
+#open INPUT,'<rever123' or die "$!";
+
+#while (defined(my $line=<INPUT>))
+#    {
+
+#    print OUTPUT $line;
+    #print reverse (OUTPUT $line);
+
+#    }
+
+my @lines = <STDIN>;
+
+foreach my $line (reverse @lines) {
+    print OUTPUT $line;
+}
+
+#close INPUT;
+close OUTPUT;
+
+
